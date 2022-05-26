56522d85c0d4c2a785e2e2d8367558f29cc81fbc
aimarwibowo46 <aimarwibowo46@gmail.com>
2022-05-26 15:28:50 +0700

    Implement layout for Camera Activity

--- app/src/main/java/com/traveloka/ocr/VerificationActivity.kt
+++ app/src/main/java/com/traveloka/ocr/VerificationActivity.kt
@@ -1,11 +1,19 @@
 package com.traveloka.ocr
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.traveloka.ocr.databinding.ActivityVerificationBinding
+import java.io.File
 
 class VerificationActivity : AppCompatActivity() {
 
@@ -17,6 +25,16 @@ class VerificationActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         title = getString(R.string.verify_ktp)
+
+        if (!allPermissionsGranted()) {
+            ActivityCompat.requestPermissions(
+                this,
+                REQUIRED_PERMISSIONS,
+                REQUEST_CODE_PERMISSIONS
+            )
+        }
+
+        binding.btnCamera.setOnClickListener { startCameraX() }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -48,5 +66,49 @@ class VerificationActivity : AppCompatActivity() {
             }
         }
         return true
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (!allPermissionsGranted()) {
+                Toast.makeText(
+                    this,
+                    "Permission is not granted.",
+                    Toast.LENGTH_SHORT
+                ).show()
+                finish()
+            }
+        }
+    }
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+
+    private fun startCameraX() {
+        val intent = Intent(this, CameraActivity::class.java)
+        launcherIntentCameraX.launch(intent)
+    }
+
+    private val launcherIntentCameraX = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        if (it.resultCode == CAMERA_X_RESULT) {
+            val myFile = it.data?.getSerializableExtra("picture") as File
+            val result = BitmapFactory.decodeFile(myFile.path)
+
+            binding.imgCard.setImageBitmap(result)
+        }
+    }
+
+    companion object {
+        const val CAMERA_X_RESULT = 200
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+        private const val REQUEST_CODE_PERMISSIONS = 10
     }
 }
