cd69cae7c2600e9026c32c6df06051c5b1d3373f
baskom99 <rizkynuansa2@gmail.com>
2022-05-28 00:10:38 +0700

    add progressbar

--- app/src/main/java/com/traveloka/ocr/LoginActivity.kt
+++ app/src/main/java/com/traveloka/ocr/LoginActivity.kt
@@ -2,10 +2,24 @@ package com.traveloka.ocr
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.traveloka.ocr.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_login)
+    }
+
+    private fun showLoading(isLoading: Boolean) {
+        if (isLoading) {
+            binding.progressBar.visibility = View.VISIBLE
+        } else {
+            binding.progressBar.visibility = View.GONE
+        }
     }
 }
