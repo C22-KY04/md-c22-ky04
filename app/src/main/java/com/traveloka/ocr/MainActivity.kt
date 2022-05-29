97eeffe3e11fc9ceeea1cb99d45cbb65714b1699
aimarwibowo46 <aimarwibowo46@gmail.com>
2022-05-29 23:01:16 +0700

    Add search menu in Main Activity and add some color asset

--- app/src/main/java/com/traveloka/ocr/MainActivity.kt
+++ app/src/main/java/com/traveloka/ocr/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.traveloka.ocr
 
+import android.app.SearchManager
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.widget.SearchView
 import com.traveloka.ocr.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,10 +19,47 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
 
-//        binding.btLogin.setOnClickListener {
-//            val intent = Intent(this, VerificationActivity::class.java)
-//            startActivity(intent)
-//        }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.option_menu, menu)
+
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchMenu = menu.findItem(R.id.menu_search).actionView as SearchView
+
+        searchMenu.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        searchMenu.queryHint = resources.getString(R.string.search_hint)
+        searchMenu.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                Toast.makeText(this@MainActivity, query, Toast.LENGTH_SHORT).show()
+                searchMenu.clearFocus()
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return false
+            }
+        })
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.menu_verify -> {
+                val intent = Intent(this, VerificationActivity::class.java)
+                startActivity(intent)
+            }
+
+            R.id.menu_languages -> {
+                //
+            }
+
+            R.id.menu_logout -> {
+                //
+            }
+        }
+        return true
     }
 }
